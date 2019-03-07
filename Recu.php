e0351d45ccc63c29aaa9dbbcf78f386dcc473c4e
KohutDM <47785589+KohutDM@users.noreply.github.com>
2019-03-07 13:19:42 +0200

    Update Recu.php

--- Recu.php
+++ Recu.php
@@ -2,22 +2,21 @@
 $i=0;
 
 /**
- * @param type array $w
+ * @param type array $my_array
  * @param type int $i
  * @return type string
  */
 function recu ($array, &$i)
 {
     for ($i;$i<=1;$i++){
-    if (!is_array ($array)){
-        return $array . "<br/>";
-    }
-    $result=null;    
-    foreach ($array as $array_part){
-        $result .= recu($array_part, &$i);
-    }
-    
-    return $result;
+        if (!is_array ($array)){
+            return $array . "<br/>";
+        }
+        $result=null;    
+        foreach ($array as $array_part){
+            $result .= recu($array_part, &$i);
+        }
+        return $result;
     }    
 }
 $my_array = array(1,2,3,4, array(51,52,53, array(541,542, array(5421,5422, array(54221)))),6,7, array(81,82,83));
